ecdb748198b6f0e61a77a595d4d7b837aba2d337
yarikbely <159477291+yarikbely@users.noreply.github.com>
2025-10-30 21:52:56 +0200

    LAB3

--- main.c
+++ main.c
@@ -1,25 +1,35 @@
 #include <stdint.h>
 #include <stm32f10x.h>
 
-void delay(uint32_t ticks) {
-	for (int i=0; i<ticks; i++) {
-		__NOP();
-	}
+void delay(volatile uint32_t ticks) {
+    for (volatile uint32_t i = 0; i < ticks; i++) {
+        __NOP();
+    }
 }
 
-int __attribute((noreturn)) main(void) {
-	// Enable clock for AFIO
-	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-	// Enable clock for GPIOC
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	// Enable PC13 push-pull mode
-	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+int __attribute__((noreturn)) main(void) {
+
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN; 
+
+
+    GPIOC->CRH &= ~GPIO_CRH_CNF13;      
+    GPIOC->CRH |= GPIO_CRH_MODE13_0;     
+
+
+    GPIOA->CRL &= ~(GPIO_CRL_CNF0 | GPIO_CRL_MODE0); 
+    GPIOA->CRL |= GPIO_CRL_CNF0_1;                   
+    GPIOA->ODR |= GPIO_ODR_ODR0;                   
 
     while (1) {
-	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
-		delay(1000000);
-	    GPIOC->ODR &= ~(1U<<13U);
-	    delay(1000000);
+        uint32_t button_pressed = !(GPIOA->IDR & GPIO_IDR_IDR0);
+
+        uint32_t delay_val = button_pressed ? 200000 : 1000000;
+
+        GPIOC->ODR &= ~(1U << 13);  
+        delay(delay_val);
+
+        GPIOC->ODR |= (1U << 13);  
+        delay(delay_val);
     }
 }
